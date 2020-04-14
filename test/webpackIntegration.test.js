56a16c6dd3d97112c261103e4534c8c7baecf417
xdnloveme <530063113@qq.com>
2020-04-14 17:28:02 +0800

    test: fix test bug

--- test/webpackIntegration.test.js
+++ test/webpackIntegration.test.js
@@ -1,48 +1,56 @@
 const webpack = require('webpack');
 const path = require('path');
 const fs = require('fs');
-const webpackConfig = require('./webpack-build-test/webpack.config');
-const Svg2IconfontWebpack = require('../index');
+const Svg2IconfontWebpack = require('..');
 
-const resolve = (current) => {
+jest.setTimeout(30000);
+
+const resolve = current => {
   return path.resolve(__dirname, current);
-}
+};
+
+const webpackConfig = require(resolve('./webpack-build-test/webpack.config'));
 
 describe('Webpack Integration Tests', () => {
-  test('build', done => {
-        webpack(webpackConfig, (err, stats) => {
-      if (err || stats.hasErrors()) {
-        return done(err || stats.toString());
+  it('build', done => {
+    const callback = (err, stats) => {
+      try {
+        console.log('哈哈', stats.hasErrors());
+        if (err || stats.hasErrors()) {
+          return done(err || stats.toString());
+        }
+        const webpackOutFilename = webpackConfig.output.filename;
+        const expectedDistDir = ['css', 'index.html', webpackOutFilename];
+        const distDir = fs.readdirSync(resolve('./webpack-build-test/dist'));
+        const distFontFiles = fs.readdirSync(resolve('./webpack-build-test/dist/css'));
+        const pluginsFilter = webpackConfig.plugins.filter(each => each instanceof Svg2IconfontWebpack);
+  
+        // distDir equal expectedDistDir
+        expect(distDir).toEqual(expectedDistDir);
+  
+        // Svg2IconfontWebpackPlugins length > 0
+        expect(pluginsFilter.length > 0).toBeTruthy();
+  
+        const iconfontSuffix = ['eot', 'svg', 'ttf', 'woff', 'woff2'];
+        const Svg2IconfontWebpackPlugins = pluginsFilter[0];
+        // iterator for each
+        const iterator = iconfontSuffix.entries();
+        let each = iterator.next();
+        while (!each.done) {
+          const fileName = Svg2IconfontWebpackPlugins.pluginOptions.output.fileName;
+          // check each iconfont output
+          expect(distFontFiles.includes(`${fileName}.${each.value[1]}`)).toBeTruthy();
+          each = iterator.next();
+        }
+        const cssFileName = Svg2IconfontWebpackPlugins.pluginOptions.output.cssFileName;
+        expect(distFontFiles.includes(`${cssFileName}.css`)).toBeTruthy();
+  
+        done();
+      } catch (e) {
+        done(e);
       }
-      const webpackOutFilename = webpackConfig.output.filename;
-      const expectedDistDir = ['css', 'index.html', webpackOutFilename];
-      const distDir = fs.readdirSync(resolve('./webpack-build-test/dist'))
-      const distFontFiles = fs.readdirSync(resolve('./webpack-build-test/dist/css'));
-      const pluginsFilter = webpackConfig.plugins.filter(each => each instanceof Svg2IconfontWebpack);
+    }    
 
-      // distDir equal expectedDistDir
-      expect(distDir).toEqual(expectedDistDir);
-      
-      // Svg2IconfontWebpackPlugins length > 0
-      expect(pluginsFilter.length > 0).toBeTruthy();
-
-      const iconfontSuffix = ['eot', 'svg', 'ttf', 'woff', 'woff2'];
-      const Svg2IconfontWebpackPlugins = pluginsFilter[0];
-      // iterator for each
-      const iterator = iconfontSuffix.entries();
-      let each = iterator.next();
-      while(!each.done) {
-        
-        const fileName = Svg2IconfontWebpackPlugins.pluginOptions.output.fileName;
-        // check each iconfont output
-        expect(distFontFiles.includes(`${fileName}.${each.value[1]}`)).toBeTruthy();
-        each = iterator.next();
-      }
-      const cssFileName = Svg2IconfontWebpackPlugins.pluginOptions.output.cssFileName;
-      expect(distFontFiles.includes(`${cssFileName}.css`)).toBeTruthy();
-
-      done();
-    });
-    
-  }, 30000);
+    webpack(webpackConfig, callback);
+  });
 });
