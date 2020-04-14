fed215ae0c831b3e536ec6fb101e8a8a620128a9
xdnloveme <530063113@qq.com>
2020-04-14 17:27:46 +0800

    chore: update version

--- test/webpack-build-test/webpack.config.js
+++ test/webpack-build-test/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const Svg2IconfontWebpack = require('../../index');
+const Svg2IconfontWebpack = require('../..');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const resolve = current => {
@@ -8,14 +8,44 @@ const resolve = current => {
 
 module.exports = {
   mode: 'production',
-  entry: './index',
+  entry: './index.js',
   output: {
     path: __dirname + '/dist',
+    publicPath: '/',
     filename: 'index_bundle.js',
+  },
+  optimization: {
+    minimize: false
+  },
+  module: {
+    unknownContextCritical: false,
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".json"],
+    modules: ["node_modules"]
+  },
+  node: {
+    fs: 'empty',
+    child_process: 'empty'
   },
   target: 'node',
   plugins: [
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: resolve('./index.html')
+    }),
     new Svg2IconfontWebpack({
       assetsPath: resolve('./assets'),
     }),
